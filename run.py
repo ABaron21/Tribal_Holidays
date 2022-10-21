d608ba49b21e8e5d6f75ede12be27e2358a999f3
A_Baron <abaron756@gmail.com>
2022-10-21 14:25:02 +0000

    Tidy run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from holidaymanager import app
 
 if __name__ == "__main__":
     app.run(
-        host = os.environ.get("IP"),
-        port = int(os.environ.get("PORT")),
-        debug = os.environ.get("DEBUG")
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
     )
