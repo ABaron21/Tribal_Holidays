5b60e5a1beebdeb030538e64e34955b8c6d44057
A_Baron <abaron756@gmail.com>
2022-10-24 13:55:55 +0000

    Add register page with structure and styling

--- holidaymanager/routes.py
+++ holidaymanager/routes.py
@@ -6,3 +6,8 @@ from holidaymanager.models import Users, Caravans, Events, Bookings
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
